feea6bef8b6874319d0a78f43369964a14828c1e
Simon Fieber <si0nDE@users.noreply.github.com>
2018-01-15 12:15:06 +0100

    Erkennung für Mobilgeräte
    Mobile Geräte erhalten nun reCaptcha und alle anderen Coinhive.
    
    Konfiguration kann in der config.php vorgenommen werden.

--- contact-form/config/config.php
+++ contact-form/config/config.php
@@ -2,22 +2,30 @@
 
 return [
     'subject' => [
-        'prefix' => '[Kontaktformular]'
+		'prefix' => '[Kontaktformular]'
     ],
     'emails' => [
-        'to'   => '',
-        'from' => ''
+		'to'   => 'REPLACE_ME',
+		'from' => 'REPLACE_ME'
     ],
     'messages' => [
-        'error'   => 'Beim Senden ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.',
-        'success' => 'Ihre Nachricht wurde erfolgreich gesendet.'
+		'error'   => 'Beim Senden ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.',
+		'success' => 'Ihre Nachricht wurde erfolgreich gesendet.'
     ],
     'fields' => [
-        'name'     => 'Name',
-        'email'    => 'E-Mail-Adresse',
-        'phone'    => 'Telefon',
-        'subject'  => 'Betreff',
-        'message'  => 'Nachricht',
-        'btn-send' => 'Senden'
-    ]
+		'name'     => 'Name',
+		'email'    => 'E-Mail-Adresse',
+		'phone'    => 'Telefon',
+		'subject'  => 'Betreff',
+		'message'  => 'Nachricht',
+		'btn-send' => 'Senden'
+    ],
+	'coinhive' => [
+		'data-key'   => 'Dj5PVUOom6w00NhLZPfsWlZlmzKhdT1H',	/* Site Key (Public) von https://coinhive.com */
+		'data-hashes' => '1024',							/* Anzahl der Hashes, die vom Mining-Pool akzeptiert werden müssen. Das Ziel sollte ein Vielfaches von 256 sein. */
+		'data-whitelabel' => 'false'						/* Coinhive-Logo und "What is this?"-Link ausblenden  */
+    ],
+	'recaptcha' => [
+		'data-sitekey'   => 'REPLACE_ME',					/* Websiteschlüssel von https://www.google.com/recaptcha/ */
+    ],
 ];
